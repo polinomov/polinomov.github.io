1a4f3d97c9a569b8e393aa4a929af366770702da
votslot <mpolotskaia64@gmail.com>
2024-04-29 01:53:18 -0700

    blah-blah

--- js_helpers.js
+++ js_helpers.js
@@ -68,24 +68,32 @@ function OnFileSelected(input) {
 }
 
 function OnSampleLoad(){
-    console.log("OnSampleLoad");
+    console.log("-OnSampleLoad-");
     const req = new XMLHttpRequest();
-    //req.open("GET", "https://drive.google.com/file/d/1HYSlnX1xQ79pwgYSU50yz7uguVv9iSpb/view?usp=sharing", true);
+    //req.open("GET", "https://drive.google.com/file/d/1HYSlnX1xQ79pwgYSU50yz7uguVv9iSpb/view?usp=sharing");
     req.open("GET", "/sample.las", true);
+    /*
     req.responseType = "arraybuffer";
-
-    req.onload = (event) => {
+     req.onload = (event) => {
         const arrayBuffer = req.response; // Note: not req.responseText
         if (arrayBuffer) {
             const byteArray = new Uint8Array(arrayBuffer);
                 console.log(" here byteArray" + byteArray.length)
                 byteArray.forEach((element, index) => {
-                // do something with each byte in the array
-                //console.log(" here byteArray")
-            });
+             });
         }
     };
-    req.send(null);
+    req.send();
+    */
+    req.onload = function() {
+        var content = req.responseText;
+        console.log(" Download Size" + content.length);
+         console.log("Yes Download " + content);
+    };
+    req.onerror = function() {
+        alert("Download failure.");
+    };
+    req.send();
 }
 
 function OnFileOpen() {
